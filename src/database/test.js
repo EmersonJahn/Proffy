3bf49c06587e886af378c39e553a4d208c63d383
Emerson Adierson Jahn <emersonajahn@gmail.com>
2020-09-02 22:03:40 -0300

    Consulta de dados no banco
    Criado filtro por dia da semana, horário inicial e final

--- src/database/test.js
+++ src/database/test.js
@@ -12,7 +12,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: "Química", 
+        subject: 1, 
         cost: "20", 
         //proffy_id
     }
@@ -30,6 +30,32 @@ Database.then(async (db) => {
         }
     ]
 
+    // Gravar dados no banco
     // await createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+    // Consultar os dados inseridos
+
+    // todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+
+    // consultar os dados de um determinado professor e as suas classes
+    const selectedClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+
+    // filtrar por dia da semana, horário inicial e horário final
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = 1
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "520"
+        AND class_schedule.time_to > "800"
+    `)
+
+
 
 })
